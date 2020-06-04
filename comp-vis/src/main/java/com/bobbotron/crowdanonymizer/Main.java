db7d996680c09bd3a1f2de1a1bc3277cc022c4f6
Robert Young <bobbotron@gmail.com>
2020-06-04 09:37:30 -0400

    Added basic project skeleton

---
+++ comp-vis/src/main/java/com/bobbotron/crowdanonymizer/Main.java
@@ -0,0 +1,15 @@
+package com.bobbotron.crowdanonymizer;
+
+import nu.pattern.OpenCV;
+import org.opencv.core.Core;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    OpenCV.loadShared();
+	//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	
+	System.exit(0);
+  }
+}
